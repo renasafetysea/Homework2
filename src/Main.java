35b18ef3a73074e6aee8b2d72431e06636bc1f23
Anna Molodtsova <renasafetysea@mail.ru>
2022-08-03 00:01:34 +0300

    Выполнила ДЗ 2 (коректировка)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         //Задача 1
         byte apples = 100;
         short seeds = 11500;
@@ -9,64 +10,82 @@ public class Main {
         float ratWeight = 0.2f;
         char club = 106;
         boolean applesIsAdult = apples > 120;
-        System.out.println(club);
+        System.out.println(applesIsAdult);
+
         //Задача 2
         double firstBoxer = 78.2;
         double secondBoxer = 82.7;
-        double weightOfAllBoxers = firstBoxer+secondBoxer;
-        double weightDifference = secondBoxer-firstBoxer;
-        System.out.println("Общий вес " + weightOfAllBoxers);
-        System.out.println("Разница в весе " + weightDifference);
+
+        double total = firstBoxer+secondBoxer;
+
+        double difference = Math.abs(firstBoxer-secondBoxer);
+
+        System.out.println("Общий вес " + total);
+
+        System.out.println("Разница в весе " + difference);
+
         // Задача 3
-       int bananasItems = 5 ;
-       int oneBananasWeight = 80;
-       int allBananasWeight = bananasItems * oneBananasWeight;
-       int milkItems = 2;
-       int oneMilkWeight = 105;
-       int allMilkWeight = milkItems * oneMilkWeight;
-       int icecreamItems = 2;
-       int oneIcecreamWeight = 100;
-       int allIcecreamWeight = icecreamItems * oneIcecreamWeight;
-       int eggItems = 4;
-       int oneEggWeight = 70;
-       int allEggWeight = eggItems * oneEggWeight;
-       int grammInBlender = allBananasWeight + allMilkWeight + allIcecreamWeight + allEggWeight;
-       float kilogrammInBlender = grammInBlender / 1000f;
-       System.out.println("В блендере грамм " + grammInBlender);
-       System.out.println("В блендере килорамм " + kilogrammInBlender);
+       int banana = 5 ;
+       int bananasWeight = 80;
+
+       int milk = 200/100;
+       int milkWeight = 105;
+
+       int icecream = 2;
+       int icecreamWeight = 100;
+
+       int egg = 4;
+       int eggWeight = 70;
+
+       int totalB = (banana*bananasWeight) + (milk*milkWeight) + (icecream*icecreamWeight) + (egg*eggWeight);
+
+       int gramsInKg = 1000;
+
+       double totalKgs = totalB / (gramsInKg*1.0);
+
+       System.out.println("В блендере килорамм " + totalKgs);
+
      // Задача 4
-        int firstLoseWeightPlan = 250;
-        int secondLoseWeightPlan = 500;
-        int allWeightLose = 7*1000;
-        int daysFirstlwp = allWeightLose / firstLoseWeightPlan;
-        int daysSecondlwp = allWeightLose / secondLoseWeightPlan;
+        int weightLose = 7;
+        int gramsInKgLose = 1000;
+        int weightInGrams = weightLose*gramsInKgLose;
+
+        int firstWeightPlan = 250;
+        int secondWeightPlan = 500;
+
+        int daysFirstlwp = weightInGrams / firstWeightPlan;
+        int daysSecondlwp = weightInGrams/ secondWeightPlan;
         int middleNumberDays = (daysFirstlwp+daysSecondlwp) / 2;
 
         System.out.println("Если худеть на 250 гр в день нужно " + daysFirstlwp + " дней");
         System.out.println("Если худеть на 500 гр в день нужно " + daysSecondlwp + " дней");
         System.out.println("Среднее количество дней " + middleNumberDays);
+
         // Задача 5
-        int cashMonthMasha = 67760;
-        int cashMonthDenis = 83690;
-        int cashMonthKristina = 76230;
-        int cashUpMasha = ((67760 / 100) * 10) + 67760;
-        int cashUpDenis = ((83690 / 100) * 10) + 83690;
-        int cashUpKristina = ((76230 / 100) * 10) + 76230;
-        int yearCashMashaWas = 67760 * 12;
-        int yearCashDenisWas = 83690 * 12;
-        int yearCashKristinaWas = 76230 * 12;
-        int yearCashMashaNow = cashUpMasha * 12;
-        int yearCashDenisNow = cashUpDenis * 12;
-        int yearCashKristinaNow = cashUpKristina * 12;
-        int payRaiseMasha = yearCashMashaNow - yearCashMashaWas;
-        int payRaiseDenis = yearCashDenisNow - yearCashDenisWas;
-        int payRaiseKristina = yearCashKristinaNow - yearCashKristinaWas;
+        int persent = 10;
+        double multiplayer = persent / (100*1.0);
+
+        int cashMonthMasha = 67_760;
+        int cashMonthDenis = 83_690;
+        int cashMonthKristina = 76_230;
+
+        int cashUpMasha = (int) (cashMonthMasha + (cashMonthMasha* multiplayer));
+        int cashUpDenis = (int) (cashMonthDenis + (cashMonthDenis* multiplayer));
+        int cashUpKristina = (int) (cashMonthKristina + (cashMonthKristina*multiplayer));
+
+        int mashaDifference = (cashUpMasha-cashMonthMasha)*12;
+        int denisDifference = (cashUpDenis-cashMonthDenis)*12;
+        int kristinaDifference = (cashUpKristina-cashMonthKristina)*12;
+
+
         System.out.println ("Маша теперь получает " + cashUpMasha + " рублей");
-        System.out.println("Годовой доход Маши вырос на " + payRaiseMasha + " рублей");
+        System.out.println("Годовой доход Маши вырос на " + mashaDifference + " рублей");
+
         System.out.println ("Денис теперь получает " + cashUpDenis + " рублей");
-        System.out.println("Годовой доход Дениса вырос на " + payRaiseDenis + " рублей");
+        System.out.println("Годовой доход Дениса вырос на " + denisDifference + " рублей");
+
         System.out.println ("Кристина теперь получает " + cashUpKristina + " рублей");
-        System.out.println("Годовой доход Кристины вырос на " + payRaiseKristina  + " рублей");
+        System.out.println("Годовой доход Кристины вырос на " + kristinaDifference  + " рублей");
 
 
     }
